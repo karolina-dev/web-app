25033a02775099a71703116d8b71f912c0700fd6
KAROLINA SEGURA PERDOMO <89596525+karolina-dev@users.noreply.github.com>
2024-11-22 04:04:49 -0500

    Update urls.py
    Importé vista generica para cerrar sesión

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
