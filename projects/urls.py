a4fe32b7781f393120c319774e6f9f279042f387
KAROLINA SEGURA PERDOMO <89596525+karolina-dev@users.noreply.github.com>
2024-11-22 15:15:52 -0500

    Update urls.py
    Agregué pk a la vista project_detail

--- projects/urls.py
+++ projects/urls.py
@@ -7,11 +7,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Página principal
     path('login/', auth_views.LoginView.as_view(), name='login'),
-     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.signup, name='signup'),  # Página de registro
     path('projects/', views.project_list, name='project_list'),
     path('projects/create/', views.create_project, name='create_project'),
-    path('projects/<int:id>/', views.project_detail, name='project_detail'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
     path('projects/edit/<int:pk>/', views.edit_project, name='edit_project'),
     path('projects/<int:project_id>/tickets/', views.ticket_list, name='ticket_list'),
     path('projects/<int:project_id>/tickets/create/', views.create_ticket, name='create_ticket'),
