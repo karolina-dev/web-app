d764846d08c088996f6a399890aebecca43305c9
KAROLINA SEGURA PERDOMO <89596525+karolina-dev@users.noreply.github.com>
2024-11-22 04:09:40 -0500

    Update urls.py
    Cambios para cierre de sesión

--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LogoutView
+
+
+urlpatterns = [
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+]
+
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Página principal
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.signup, name='signup'),  # Página de registro
     path('projects/', views.project_list, name='project_list'),
     path('projects/create/', views.create_project, name='create_project'),
