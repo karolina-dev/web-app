7da9b69a5b1119a31c0a73405b28d0011212c56d
KAROLINA SEGURA PERDOMO <89596525+karolina-dev@users.noreply.github.com>
2024-11-22 12:38:24 -0500

    Update settings.py
    cambios en static

--- project_management/settings.py
+++ project_management/settings.py
@@ -152,13 +152,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
