82314c3aa67edd62205ec1430d3267f5455811de
KAROLINA SEGURA PERDOMO <89596525+karolina-dev@users.noreply.github.com>
2024-11-22 15:48:25 -0500

    Update settings.py
    Modifique link para templates

--- project_management/settings.py
+++ project_management/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'project_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'projects/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
