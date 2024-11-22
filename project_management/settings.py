3bc24ff51e143af08b33ca6abbfbd273a934ec47
KAROLINA SEGURA PERDOMO <89596525+karolina-dev@users.noreply.github.com>
2024-11-22 15:57:41 -0500

    Update settings.py
    configurando para static

--- project_management/settings.py
+++ project_management/settings.py
@@ -153,8 +153,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # carpeta donde están los archivos estáticos (css, js, img)
+]
+
 
 
 # Default primary key field type
